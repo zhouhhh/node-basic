876cdbdca1110041abbf64c1ce2ed83ff6d4d1f0
周行 <793745175@qq.com>
2023-08-01 21:14:19 +0800

    fix

--- koa/index.js
+++ koa/index.js
@@ -5,7 +5,9 @@ const app = new Koa()
 app.use((ctx, next) => {
     // ctx.response
 
-    ctx.response.body = 'hello world!'
+    // ctx.response.body = 'hello world!'
+    //可以省略response
+    ctx.body = 'hello world!'
 })
 
 app.listen(3000)
