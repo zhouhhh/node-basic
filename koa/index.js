aabb349e1d1a043e1050348f8ba97f4677f45f5a
周行 <793745175@qq.com>
2023-08-04 21:12:06 +0800

    koa-router

--- koa/index.js
+++ koa/index.js
@@ -1,13 +1,60 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use((ctx, next) => {
-    // ctx.response
+// //增
+// router.post('/list', (ctx, next) => [
+//     ctx.body = {
+//         ok: 1,
+//         info: 'add success'
+//     }
+// ])
+// //获取
+// router.get('/list', (ctx, next) => [
+//     ctx.body = ['111', '222', '333']
+// ])
+// //修改
+// router.put('/list/:id', (ctx, next) => [
+//     ctx.body = {
+//         ok: 1,
+//         info: 'put success'
+//     }
+// ])
+// //删除
+// router.del('/list/:id', (ctx, next) => [
+//     ctx.body = {
+//         ok: 1,
+//         info: 'delete success'
+//     }
+// ])
 
-    // ctx.response.body = 'hello world!'
-    //可以省略response
-    ctx.body = 'hello world!'
-})
+//或者简写写成以下链式调用
+//增
+router.post('/list', (ctx, next) => [
+    ctx.body = {
+        ok: 1,
+        info: 'add success'
+    }
+])
+    .get('/list', (ctx, next) => [
+        ctx.body = ['111', '222', '333']
+    ])
+    .put('/list/:id', (ctx, next) => {
+        console.log('获取参数', ctx.params);
+        ctx.body = {
+            ok: 1,
+            info: 'put success'
+        }
+    })
+    .del('/list/:id', (ctx, next) => [
+        ctx.body = {
+            ok: 1,
+            info: 'delete success'
+        }
+    ])
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
