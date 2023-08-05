0b5da9193db3a6a76bb88db8d97850239bda76e3
周行 <793745175@qq.com>
2023-08-05 13:02:27 +0800

    静态资源koa-static

--- koa/index.js
+++ koa/index.js
@@ -2,37 +2,11 @@ const Koa = require('koa')
 const router = require('./routes')
 
 const app = new Koa()
-
-// //增
-// router.post('/list', (ctx, next) => [
-//     ctx.body = {
-//         ok: 1,
-//         info: 'add success'
-//     }
-// ])
-// //获取
-// router.get('/list', (ctx, next) => [
-//     ctx.body = ['111', '222', '333']
-// ])
-// //修改
-// router.put('/list/:id', (ctx, next) => [
-//     ctx.body = {
-//         ok: 1,
-//         info: 'put success'
-//     }
-// ])
-// //删除
-// router.del('/list/:id', (ctx, next) => [
-//     ctx.body = {
-//         ok: 1,
-//         info: 'delete success'
-//     }
-// ])
-
-//或者简写写成以下链式调用
-
+const static = require('koa-static')
+const path = require('path')
 
 //应用级中间件
+app.use(static(path.join(__dirname, 'public')))
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000)
