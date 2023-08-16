86fd7cbe6b67ee32fd3087ef2489add59e53d359
周行 <793745175@qq.com>
2023-08-16 20:40:12 +0800

    MySQL增删改查

--- node-express-MySQL/index.js
+++ node-express-MySQL/index.js
@@ -4,12 +4,40 @@ const mysql2 = require('mysql2');
 
 app.get('/', async (req, res) => {
     //创建链接池，进行操作
-
     const config = getDBConfig();
     const promisePool = mysql2.createPool(config).promise()
-    const data = await promisePool.query('SELECT * FROM ev_article_cate order by name desc limit 2 offset 0')
+    const name = '科技'
+    // const data = await promisePool.query(`SELECT * FROM ev_article_cate where name="${name}" order by name desc limit 2 offset 0`)//字符串拼接的方式
+    const data = await promisePool.query(`SELECT * FROM ev_article_cate where name=? and is_delete=? order by name desc limit 2 offset 0`, [name, 1])
     console.log(data[0])
 
+    res.send({
+        ok: 1,
+        data: data[0]
+    })
+})
+app.get('/insert', async (req, res) => {
+    const config = getDBConfig();
+    const promisePool = mysql2.createPool(config).promise()
+    const data = await promisePool.query(`insert into ev_article_cate (name,alias,is_delete) values (?,?,?)`, ['化学', 'huaxue', 0])
+    res.send({
+        ok: 1,
+        data: data[0]
+    })
+})
+app.get('/update', async (req, res) => {
+    const config = getDBConfig();
+    const promisePool = mysql2.createPool(config).promise()
+    const data = await promisePool.query(`update ev_article_cate set name=?, alias=? where id=?`, ['ccsw', 'kkl', 7])
+    res.send({
+        ok: 1,
+        data: data[0]
+    })
+})
+app.get('/delete', async (req, res) => {
+    const config = getDBConfig();
+    const promisePool = mysql2.createPool(config).promise()
+    const data = await promisePool.query(`delete from ev_article_cate where id=?`, [7])
     res.send({
         ok: 1,
         data: data[0]
