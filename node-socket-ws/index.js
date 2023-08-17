4d95cc57adadbc289e40bc2b22741c13ab42ed26
周行 <793745175@qq.com>
2023-08-17 21:42:54 +0800

    socket-ws

--- node-socket-ws/index.js
+++ node-socket-ws/index.js
@@ -9,13 +9,21 @@ app.get('/', (req, res) => {
 app.listen(3001)
 
 //websocket响应
-const { WebSocketServer } = require('ws');
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.WebSocketServer
 const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
-    ws.on('message', function message(data) {
+    ws.on('message', function message(data, isBinary) {
         console.log('received: %s', data);
+
+        //转发给所有人
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN && client !== ws) {//不给自己发
+                client.send(data, { binary: isBinary });
+            }
+        })
     });
 
     ws.send('欢迎来到聊天室');
