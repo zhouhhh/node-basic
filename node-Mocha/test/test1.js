a8dd0564619b1b0323290ee3ed947f70474bbf69
周行 <793745175@qq.com>
2023-08-21 20:46:29 +0800

    mocha测试和node的断言模块assert

---
+++ node-Mocha/test/test1.js
@@ -0,0 +1,28 @@
+const sum = require('../sum')
+const assert = require('assert')
+
+// console.log(sum(1, 2, 3))
+
+// assert.strictEqual(sum(), 0)
+// assert.strictEqual(sum(1), 1)
+// assert.strictEqual(sum(1, 2), 3)
+// assert.strictEqual(sum(1, 2, 3), 6)
+
+describe('大组1测试', () => {
+    describe('小组1测试', () => {
+        it('sum() 结果返回0', () => {
+            assert.strictEqual(sum(), 10)
+        })
+        it('sum(1) 结果返回1', () => {
+            assert.strictEqual(sum(1), 1)
+        })
+    })
+    describe('小组2测试', () => {
+        it('sum(1,2) 结果返回3', () => {
+            assert.strictEqual(sum(1, 2), 3)
+        })
+        it('sum(1,2,3) 结果返回6', () => {
+            assert.strictEqual(sum(1, 2, 3), 6)
+        })
+    })
+})
