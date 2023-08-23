e5296cda4dd76c717ee34131129858c86b1db923
周行 <793745175@qq.com>
2023-08-23 21:27:37 +0800

    supertest

--- node-Mocha/app.js
+++ node-Mocha/app.js
@@ -5,4 +5,6 @@ app.use((ctx) => {
     ctx.body = '<h1>hello</h1>'
 })
 
-app.listen(3000)
+// app.listen(3000)
+
+module.exports = app
--- node-Mocha/test/test6.js
+++ node-Mocha/test/test6.js
@@ -1,9 +1,27 @@
 const assert = require('assert');
 const axios = require('axios');
 
+const supertest = require('supertest');
+const app = require('../app')
+
+//这里本地服务关闭后会报错
 describe('测试接口', () => {
     it('返回html代码片段测试', async () => {
         const res = await axios.get('http://localhost:3000/')
         assert.strictEqual(res.data, '<h1>hello</h1>')
     })
+})
+
+describe('测试接口2', () => {
+    let server
+    it('返回html代码片段测试2', async () => {
+        await supertest(server).get('/')
+            .expect('Content-Type', /text\/html/)
+            .expect(200, '<h1>hello</h1>')
+    })
+    //钩子函数
+    before(() => {
+        server = app.listen(3000)
+    })
+    after(() => server.close())//所有测试执行完后关闭服务
 })
